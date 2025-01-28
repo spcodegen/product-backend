7f272031de4033417b0fa463b7bb98d0fb81820e
spcodegen <shehansp2000@gmail.com>
2025-01-28 11:39:46 +0530

    first commit

---
+++ src/main/java/edu/icet/ecom/model/Product.java
@@ -0,0 +1,12 @@
+package edu.icet.ecom.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+
+public class Product {
+    private String name;
+    private String description;
+    private Double price;
+}
